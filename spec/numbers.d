68ccc6524193e0a2acfd3e1c00eb7dd8c4b15123
Jason Nelson <jason@carbonmade.com>
2017-02-03 20:07:33 -0600

    Create numbers.d

---
+++ spec/numbers.d
@@ -0,0 +1,33 @@
+# Numbers
+
+By default, all numeric literals are Float64.
+
+```
+let a = 1      # Float64
+let b: i32 = 1 # Int32
+```
+
+### Integers
+
+i8  Int8
+i16 Int16
+i32 Int32
+i16 Int64
+
+ui8  UInt8
+ui16 UInt16
+ui32 UInt32
+ui64 UInt64
+...
+
+### Floats
+
+f16 Float16
+f32 Float32
+f64 Float64
+
+### Decimal
+
+dec64 Dec64
+d64   Decimal64
+d128  Decimal128
