f99d9c9411961f84f8c0a439d29b15c781b1d225
Jason Nelson <jason@carbonmade.com>
2017-08-07 17:49:14 -0500

    Fix Volume units

--- modules/Geometry/Volume.d
+++ modules/Geometry/Volume.d
@@ -1,14 +1,14 @@
-ml   unit<Volume> Milliliter = 0.001 L
-L    unit<Volume> Liter      = 1
-  
-// Imperial
-oz   unit<Volume> Ounce      = 0.0295735 L
-cups unit<Volume> Cup        = 8 oz
-pt   unit<Volume> Pint       = 2 cups
-qt   unit<Volume> Quart      = 4 cups
-gal  unit<Volume> Gallon     = 4 qt
-tsp  unit<Volume> Teaspoon   = (1/6) oz
-tbsp unit<Volume> Tablespoon = 3 tsp
+ml   unit : Volume @name("Milliliter") = 0.001 L
+L    unit : Volume @name("Liter")      = 1
+
+// imperial
+oz   unit : Volume @name("Ounce")      = 0.0295735 L
+cups unit : Volume @name("Cup")        = 8 oz
+pt   unit : Volume @name("Pint")       = 2 cups
+qt   unit : Volume @name("Quart")      = 4 cups
+gal  unit : Volume @name("Gallon")     = 4 qt
+tsp  unit : Volume @name("Teaspoon")   = (1/6) oz
+tbsp unit : Volume @name("Tablespoon") = 3 tsp
 
 // CubicFoot,
 // CubicInch,
