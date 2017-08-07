1b6a69593d86732b1d5928778b7d7cdf5c2ee937
Jason Nelson <jason@carbonmade.com>
2017-08-07 17:36:23 -0500

    Fixes

--- modules/Geometry/Length.d
+++ modules/Geometry/Length.d
@@ -1,9 +1,9 @@
-# metric units
+// metric units
 cm unit : Length @name("Centimeter") @SI = 0.001 m   
 m  unit : Length @name("Meter")      @SI = 1
 km unit : Length @name("Kilometer")  @SI = 1000 m
 
-# imperial units
+// imperial units
 inch unit : Length @name("Inch") @imperial = 0.025400 m
 ft   unit : Length @name("Foot") @imperial = 12 inch
 mi   unit : Length @name("Mile") @imperial = 5280 ft
