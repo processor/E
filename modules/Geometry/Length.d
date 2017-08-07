ccab2242222a513608244a795fb13a669db4e958
Jason Nelson <jason@carbonmade.com>
2017-08-07 17:16:37 -0500

    Don’t use “in” keyword

--- modules/Geometry/Length.d
+++ modules/Geometry/Length.d
@@ -1,7 +1,9 @@
-cm unit : Length @name("Centimeter") @SI = 0.001 m      
+# metric units
+cm unit : Length @name("Centimeter") @SI = 0.001 m   
 m  unit : Length @name("Meter")      @SI = 1
 km unit : Length @name("Kilometer")  @SI = 1000 m
 
-in unit : Length @name("Inch") @imperial = 0.025400 m  
-ft unit : Length @name("Foot") @imperial = 12 in
-mi unit : Length @name("Mile") @imperial = 5280 ft
+# imperial units
+inch unit : Length @name("Inch") @imperial = 0.025400 m
+ft   unit : Length @name("Foot") @imperial = 12 inch
+mi   unit : Length @name("Mile") @imperial = 5280 ft
