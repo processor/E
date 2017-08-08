c2493deb646c137c4663217d6d3948fe734351c3
Jason Nelson <jason@carbonmade.com>
2017-08-08 12:37:57 -0500

    Cleanup area

--- modules/Geometry/Area.d
+++ modules/Geometry/Area.d
@@ -1,16 +1,18 @@
-Area protocol : Number @dimensions(2) { }
+Area protocol : Number @dimensions(2) { 
 
-Acre    unit : Area
-Are     unit : Area // 100 square metres
-Hectare unit : Area // 100 ares                  // ha
+}
 
-Barn unit : Area    //  10^−28 square meters.
+Acre    unit : Area { value: 1 } 
+Are     unit : Area { value: 1 } // 100 square metres
+Hectare unit : Area { value: 1 } // 100 ares                  // ha
 
-Tetrad unit : Area
-Hectad unit : Area
-Myriad unit : Area
+Barn unit : Area { value: 1 }  //  10^−28 square meters.
 
-SquareFoot unit : Area = ft * ft
-SquareYard unit : Area = yd * yd
-SquareMile unit : Area // sq mi
-Acre       unit : Area // 4,840 square yards = 43,560 square feet.
+Tetrad unit : Area { value: 1 }
+Hectad unit : Area { value: 1 }
+Myriad unit : Area { value: 1 }
+
+SquareFoot unit : Area { value: ft * ft }
+SquareYard unit : Area { value: yd * yd }
+SquareMile unit : Area { value: mi * mi }
+Acre       unit : Area { value: 43_560 sf }  // 4,840 square yards = 43,560 square feet.
