4133b6100b2dc8d2506361a4d4b47f721aba3689
Jason Nelson <jason@carbonmade.com>
2017-08-08 16:34:44 -0500

    Fix bezier

--- modules/geometry/Bezier.d
+++ modules/geometry/Bezier.d
@@ -5,16 +5,16 @@ Bezier type {
   c4: Vector3 // second anchor point 
 }
 
-Curve impl for Bezier impl {
+Curve impl for Bezier {
   getPoint (t: Number) => Vector2 (
     x: c1.x * b1(t) + c2.x * b2(t) + c3.x * b3(t) + c4.x * c4(t),
     y: c1.y * b1(t) + c2.y * b2(t) + c3.y * b3(t) + c4.y * b4(t)
   )
 
-  b1 (t: Number) => t * t * t
-  b2 (t: Number) => 3 * t * t * (1-t)
-  b3 (t: Number) => 3 * t * (1-t) * (1-t)
-  b4 (t: Number) => (1 - t) * (1 - t) * (1 - t)
+  private b1 (t: Number) => t * t * t
+  private b2 (t: Number) => 3 * t * t * (1-t)
+  private b3 (t: Number) => 3 * t * (1-t) * (1-t)
+  private b4 (t: Number) => (1 - t) * (1 - t) * (1 - t)
 }
 
 // Usage
