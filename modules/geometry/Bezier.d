0bd7fc9dbbfa6df2278408d581b3c26f20229544
Jason Nelson <jason@carbonmade.com>
2018-01-11 12:01:44 -0600

    Introduce private blocks

--- modules/geometry/Bezier.d
+++ modules/geometry/Bezier.d
@@ -4,15 +4,17 @@ Bezier<T: Numeric & Blittable = Float64> struct : Curve {
   c3: (x: T, y: T) // second control point
   c4: (x: T, y: T) // second anchor point 
 
-  private b1 (t: T) => t * t * t
-  private b2 (t: T) => 3 * t * t * (1-t)
-  private b3 (t: T) => 3 * t * (1-t) * (1-t)
-  private b4 (t: T) => (1 - t) * (1 - t) * (1 - t)
-
   getPoint (t: T) => Vector2 (
     x: c1.x * b1(t) + c2.x * b2(t) + c3.x * b3(t) + c4.x * c4(t),
     y: c1.y * b1(t) + c2.y * b2(t) + c3.y * b3(t) + c4.y * b4(t)
   )
+  
+  private {
+    b1 (t: T) => t * t * t
+    b2 (t: T) => 3 * t * t * (1-t)
+    b3 (t: T) => 3 * t * (1-t) * (1-t)
+    b4 (t: T) => (1 - t) * (1 - t) * (1 - t)
+  }
 }
 
 // # Usage
