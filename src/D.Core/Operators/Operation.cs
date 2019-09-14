11c63bebbdbde2689d935e697fb2bc6f75cfc2d4
Jason Nelson <jason@carbonmade.com>
2019-09-14 11:36:53 -0700

    Rename Verb to Operation

---
+++ src/D.Core/Operators/Operation.cs
@@ -0,0 +1,24 @@
+﻿namespace D
+{
+    public readonly struct Operation : IObject
+    {
+        public Operation(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        ObjectType IObject.Kind => ObjectType.Operation;
+    }
+
+    // provide
+    // own          | have (something) as one's own
+    // bill 
+    // read
+    // write
+
+    // ? May | Must | Will | Is
+}
+
+// AKA action
