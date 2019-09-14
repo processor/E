5009a52b5ab961475284a3bf9281bd63d36d9c7d
Jason Nelson <jason@carbonmade.com>
2019-09-14 12:25:54 -0700

    Add Associativity

---
+++ src/D.Core/Operators/Associativity.cs
@@ -0,0 +1,9 @@
+﻿namespace D
+{
+    // Determines the order in which operators with the same precedence are processed
+    public enum Associativity : byte
+    {
+        Left = 1,
+        Right = 2
+    }
+}
