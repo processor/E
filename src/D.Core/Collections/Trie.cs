db3c2adf5d504981180148a31791e5bc622911a5
Jason Nelson <jason@carbonmade.com>
2019-09-14 12:13:40 -0700

    Format Trie

--- src/D.Core/Collections/Trie.cs
+++ src/D.Core/Collections/Trie.cs
@@ -39,8 +39,6 @@ namespace D.Collections
 
         public void Add(string key, T value)
         {
-            if (key == null) throw new ArgumentNullException(nameof(key));
-
             var node = Root;
 
             foreach (var character in key)
@@ -48,15 +46,17 @@ namespace D.Collections
                 node = node.Add(character);
             }
 
-            if (node.IsLeaf) throw new ArgumentException($"Key {key} already exists");
+            if (node.IsLeaf)
+            {
+                throw new ArgumentException($"Key {key} already exists");
+            }
 
             node.Value = value;
 
             count++;
         }
 
-        public bool ContainsKey(string key)
-            => TryGetValue(key, out T value);
+        public bool ContainsKey(string key) => TryGetValue(key, out _);
 
         public IEnumerable<(string, T)> Scan(string prefix)
         {
@@ -75,8 +75,6 @@ namespace D.Collections
 
         public bool Remove(string key)
         {
-            if (key == null) throw new ArgumentNullException(nameof(key));
-
             if (!TryGetNode(key, out Node node))
             {
                 return false;
@@ -94,8 +92,6 @@ namespace D.Collections
 
         public bool TryGetValue(string key, out T value)
         {
-            if (key == null) throw new ArgumentNullException(nameof(key));
-
             if (!TryGetNode(key, out Node node) || !node.IsLeaf)
             {
                 value = default;
@@ -131,12 +127,12 @@ namespace D.Collections
 
         #region Node
 
-        public class Node
+        public sealed class Node
         {
             private T value;
             private bool isLeaf = false;
 
-            internal Node(char character, Node parent)
+            internal Node(char character, Node? parent)
             {
                 Character = character;
                 Parent = parent;
@@ -176,7 +172,7 @@ namespace D.Collections
                 }
             }
 
-            private Node Parent { get; }
+            private Node? Parent { get; }
 
             public Dictionary<char, Node> Children = new Dictionary<char, Node>();
 
@@ -253,8 +249,7 @@ namespace D.Collections
 
             public bool Contains(char key) =>  Children.ContainsKey(key);
 
-            public bool TryGetNode(char key, out Node node) =>
-                Children.TryGetValue(key, out node);
+            public bool TryGetNode(char key, out Node node) => Children.TryGetValue(key, out node);
         }
 
         #endregion
