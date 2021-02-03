41e47705931d4663ca011b5060dea3fbae385c59
Jason Nelson <jason@carbonmade.com>
2021-02-03 14:11:25 -0800

    Add custom operator test

---
+++ src/D.Scripting.Tests/Evaluator/CustomOperatorTests.cs
@@ -0,0 +1,23 @@
+﻿using D.Mathematics;
+
+using Xunit;
+
+namespace D.Tests
+{
+    public class CustomOperatorTests
+    {
+        [Fact]
+        public void Multiply()
+        {
+            var env = new Node();
+
+            env.Operators.Add(Operator.Infix(ObjectType.MultiplyExpression, "×", precedence: 14));
+
+            env.Add("×", new ArithmethicFunction("multiply", Arithmetic.Multiply));
+
+            Assert.Equal("0",     Script.Evaluate("0 × 10",    env).ToString());
+            Assert.Equal("1",     Script.Evaluate("1 × 1",     env).ToString());
+            Assert.Equal("100m²", Script.Evaluate("10m × 10m", env).ToString());
+        }
+    }
+}
