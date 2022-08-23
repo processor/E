34bcc5adb249f74d6e8ec5dccfcd027a262932c0
Jason Nelson <jason@carbonmade.com>
2022-08-23 09:41:04 -0700

    [Imaging] Use file scoped namespaces

--- src/D.Imaging/Color/ColorFunctions.cs
+++ src/D.Imaging/Color/ColorFunctions.cs
@@ -1,60 +1,59 @@
 ﻿using System;
 using Carbon.Color;
 
-namespace E.Imaging
+namespace E.Imaging;
+
+public class ColorFunctions
 {
-    public class ColorFunctions
-    {
-        // Hue
-        // Saturation
-        // Lightness
-        // Whiteness
-        // Blackness
-        // Tint
-        // Shade
-        // Blend
-        // Blenda
-        // Contrast
+    // Hue
+    // Saturation
+    // Lightness
+    // Whiteness
+    // Blackness
+    // Tint
+    // Shade
+    // Blend
+    // Blenda
+    // Contrast
 
-        ColorFunction AdjustAlpha(IColor source, double amount)      => new ColorFunction("alpha");
-        ColorFunction AdjustHue(IColor source, double amount)        => new ColorFunction("hue");
-        ColorFunction AdjustSaturation(IColor source, double amount) => new ColorFunction("saturation");
-        ColorFunction AdjustLightness(IColor source, double amount)  => new ColorFunction("lightness");
-        ColorFunction AdjustWhiteness(IColor source, double amount)  => new ColorFunction("whiteness");
-        ColorFunction AdjustBlackness(IColor source, double amount)  => new ColorFunction("blackness");
-        ColorFunction AdjustTint(IColor source, double amount)       => new ColorFunction("tint");
-        ColorFunction AdjustShade(IColor source, double amount)      => new ColorFunction("shade");
-        ColorFunction Blend(IColor lhs, IColor rhs, double amount)   => new ColorFunction("blend");
-        ColorFunction Blenda(IColor source, double amount)           => new ColorFunction("blenda");
-        ColorFunction AdjustContrast(IColor source, double amount)   => new ColorFunction("contrast");
-    }
-
-    public sealed class ColorFunction : IFunction
-    {
-        private readonly Func<IColor, double, IColor>? func;
-
-        public ColorFunction(string name, Func<IColor, double, IColor>? func = null)
-        {
-            Name = name;
-
-            this.func = func;
-        }
-
-        public Parameter[] Parameters => throw new NotImplementedException();
-
-        public string Name { get; }
-
-        public ObjectType Kind => throw new NotImplementedException();
-
-        public object Invoke(IArguments args)
-        {
-            var color = args[0];
-            
-            return color;
-        }
-    }
-
-
-    // color-mod(#937b19 contrast(25%)
-    // color-mod(var(--base-color) tint(59%))
+    ColorFunction AdjustAlpha(IColor source, double amount)      => new ColorFunction("alpha");
+    ColorFunction AdjustHue(IColor source, double amount)        => new ColorFunction("hue");
+    ColorFunction AdjustSaturation(IColor source, double amount) => new ColorFunction("saturation");
+    ColorFunction AdjustLightness(IColor source, double amount)  => new ColorFunction("lightness");
+    ColorFunction AdjustWhiteness(IColor source, double amount)  => new ColorFunction("whiteness");
+    ColorFunction AdjustBlackness(IColor source, double amount)  => new ColorFunction("blackness");
+    ColorFunction AdjustTint(IColor source, double amount)       => new ColorFunction("tint");
+    ColorFunction AdjustShade(IColor source, double amount)      => new ColorFunction("shade");
+    ColorFunction Blend(IColor lhs, IColor rhs, double amount)   => new ColorFunction("blend");
+    ColorFunction Blenda(IColor source, double amount)           => new ColorFunction("blenda");
+    ColorFunction AdjustContrast(IColor source, double amount)   => new ColorFunction("contrast");
 }
+
+public sealed class ColorFunction : IFunction
+{
+    private readonly Func<IColor, double, IColor>? func;
+
+    public ColorFunction(string name, Func<IColor, double, IColor>? func = null)
+    {
+        Name = name;
+
+        this.func = func;
+    }
+
+    public Parameter[] Parameters => throw new NotImplementedException();
+
+    public string Name { get; }
+
+    public ObjectType Kind => throw new NotImplementedException();
+
+    public object Invoke(IArguments args)
+    {
+        var color = args[0];
+        
+        return color;
+    }
+}
+
+
+// color-mod(#937b19 contrast(25%)
+// color-mod(var(--base-color) tint(59%))
--- src/D.Imaging/Color/RgbConstructor.cs
+++ src/D.Imaging/Color/RgbConstructor.cs
@@ -1,32 +1,31 @@
 ﻿using Carbon.Color;
 
-namespace E.Imaging
+namespace E.Imaging;
+
+public sealed class RgbConstructor : IFunction
 {
-    public sealed class RgbConstructor : IFunction
+    public Parameter[] Parameters => throw new System.NotImplementedException();
+
+    public string Name => "rgb";
+
+    public ObjectType Kind => throw new System.NotImplementedException();
+
+    public object Invoke(IArguments args)
     {
-        public Parameter[] Parameters => throw new System.NotImplementedException();
-
-        public string Name => "rgb";
-
-        public ObjectType Kind => throw new System.NotImplementedException();
-
-        public object Invoke(IArguments args)
-        {
-            return new Rgba128f(
-               r: (float)((INumber)(args[0])).Real,
-               g: (float)((INumber)(args[1])).Real,
-               b: (float)((INumber)(args[2])).Real
-           );
-        }
+        return new Rgba128f(
+           r: (float)((INumber)(args[0])).Real,
+           g: (float)((INumber)(args[1])).Real,
+           b: (float)((INumber)(args[2])).Real
+       );
     }
-
-    /*
-    rgb() = rgb( <rgb-component>#{3} )
-    rgba() = rgba( <rgb-component>#{3} , <alpha-value> )
-    <rgb-component> = <number> | <percentage>
-    <alpha-value> = <number> | <percentage>
-    */
 }
+
+/*
+rgb() = rgb( <rgb-component>#{3} )
+rgba() = rgba( <rgb-component>#{3} , <alpha-value> )
+<rgb-component> = <number> | <percentage>
+<alpha-value> = <number> | <percentage>
+*/
 
 
 /*
