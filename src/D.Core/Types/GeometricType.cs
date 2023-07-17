8fd34b35c933451777cee9ae806a0ceaa573450e
Jason Nelson <jason@carbonmade.com>
2023-07-17 09:37:28 -0700

    Format GeometricType

--- src/D.Core/Types/GeometricType.cs
+++ src/D.Core/Types/GeometricType.cs
@@ -2,28 +2,28 @@
 
 public enum GeometricType
 {
-    Arc             = 147_572,
-    Curve           = 161_973,
-    Line            = 37_105,
-    Path            = 1_415_372, // Point *
-    Plane           = 17_285,
-    Point           = 44_946, // Vector | (x,y,z?)
-    Polygon         = 37_555, // Path Point * 
-    Quaternion      = 173_853,
-    Ray             = 1_147_832, // (position: Vector3, direction: Vector3)
+    Arc        = 147_572,
+    Curve      = 161_973,
+    Line       = 37_105,
+    Path       = 1_415_372, // Point *
+    Plane      = 17_285,
+    Point      = 44_946, // Vector | (x,y,z?)
+    Polygon    = 37_555, // Path Point * 
+    Quaternion = 173_853,
+    Ray        = 1_147_832, // (position: Vector3, direction: Vector3)
 
     // 2D
-    Circle          = 17_278,
-    Ellipse         = 40_112,
-    Triangle        = 19_821,
-    Rectangle       = 209, 
+    Circle     = 17_278,
+    Ellipse    = 40_112,
+    Triangle   = 19_821,
+    Rectangle  = 209, 
 
     // 3D
-    Cone            = 42_344,
-    Cylinder        = 34_132,
-    Ellipsoid       = 190_046,
-    Prism           = 180_544,
-    Pyramid         = 3_358_290,
-    Sphere          = 12_507,
-    Torus           = 12_510,
+    Cone       = 42_344,
+    Cylinder   = 34_132,
+    Ellipsoid  = 190_046,
+    Prism      = 180_544,
+    Pyramid    = 3_358_290,
+    Sphere     = 12_507,
+    Torus      = 12_510,
 }
