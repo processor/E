c38e86b665d25ace22e7e57849e3403f4b3251bc
Jason Nelson <jason@carbonmade.com>
2023-07-17 09:37:13 -0700

    Update release path

--- build.ps1
+++ build.ps1
@@ -1,4 +1,2 @@
-# Build all
-
-dotnet pack ./src/D.Core      -c Release -o ./release/
-dotnet pack ./src/D.Scripting -c Release -o ./release/
+dotnet pack ./src/D.Core      -c Release -o ./releases/
+dotnet pack ./src/D.Scripting -c Release -o ./releases/
