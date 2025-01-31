4ebfcd78a0417cad874ce15219ebfae0498d4902
Jason Nelson <jason@carbonmade.com>
2025-01-31 13:06:44 -0800

    Update Dimension ids

--- src/D.Core/Units/Dimension.cs
+++ src/D.Core/Units/Dimension.cs
@@ -14,37 +14,38 @@ public enum Dimension
     LuminousIntensity           = 7,   // candela       cd        .7
 
     // Secondary (derived) dimensions -------------------------
-    Acceleration                = 20,  //               a       m/s²
-    Angle                       = 21,  // radian        rad
-    Frequency                   = 22,  // hertz         Hz
-    Force                       = 23,  // newton        N       kg/m/s²
-    Pressure                    = 24,  // pascal        Pa      force / area
-    SolidAngle                  = 25,  // steradian     sr
+    Acceleration                = 11376,  //            a       m/s²
+    Angle                       = 21,     // radian     rad
+    Frequency                   = 11652,  // hertz      Hz
+    Force                       = 11402,  // newton     N       kg/m/s²
+    Pressure                    = 39552,  // pascal     Pa      force / area
+    SolidAngle                  = 208476, // steradian  sr
+    Volume                      = 39297,  //            V       m^2
 
     // Electric
-    Energy                      = 30, // joule          zJ
-    ElectricCharge              = 31, // coulomb        C
-    ElectricPotentialDifference = 32, // volt           V
-    ElectricResistance          = 33, // ohm            Ω
-    ElectricConductance         = 34, // siemens        S
-    Inductance                  = 35, // henry          H
-    Capacitance                 = 36, // farad          F
-    MagneticFlux                = 37, // weber          Wb
-    MagneticFluxDensity         = 38, // tesla          T
-    Power                       = 39, // watt           W       energy/time
+    Energy                      = 30,      // joule     zJ
+    ElectricCharge              = 1111,    // coulomb   C
+    ElectricPotentialDifference = 32,      // volt      V
+    ElectricResistance          = 25358,   // ohm       Ω
+    ElectricConductance         = 34,      // siemens   S
+    Inductance                  = 35,      // henry     H
+    Capacitance                 = 36,      // farad     F
+    MagneticFlux                = 177831,  // weber     Wb
+    MagneticFluxDensity         = 38,      // tesla     T
+    Power                       = 39,      // watt      W       energy/time
+                                           
+    Illuminance                 = 194411,  // lux       lx          (= lm/m²)
+    LuminousFlux                = 41,      // lumen     lm
+                                         
+    CatalyticActivity           = 1735592, // katal     kat
 
-    Illuminance                 = 40, // lux            lx          (= lm/m²)
-    LuminousFlux                = 41, // lumen          lm
-
-    CatalyticActivity           = 50, // katal        kat
-
-    Wavenumber                  = 60, //              σ
+    Wavenumber                  = 192510, //            σ
 
     // CSS
     Resolution                  = 70,
 
     // Information
-    AmountOfInformation         = 80
+    AmountOfInformation         = 105666562
 }
 
 
